16d0b1233716c3e4c2ba4b6c9bfb0c49736571fc
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-02-17 04:45:41 +0000

    Initial viable product

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # google-image-finder
+Find images related to a topic from google. The images are loaded and written to a folder caller ``images_{query}``
+
+---
+## Setup
+    pip install -r requirements.txt
+
+## Run
+    python main.py
+
+## Inputs
+`Search query`: The topic you want images of
+
+`Number of images`: Number of images you want
---
+++ main.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+import os
+
+query = input("Search query: ")
+imgs = int(input("Number of images: "))
+url = f"https://www.google.com/search?q={query}&tbm=isch"
+
+if os.path.exists(f"images_{query}"):
+    for filename in os.listdir(f"images_{query}"):
+        os.remove(os.path.join(f"images_{query}", filename))
+    os.rmdir(f"images_{query}")
+os.mkdir(f"images_{query}")
+
+res = BeautifulSoup(requests.get(url).text)
+
+i = 0
+for img in res.find_all("img"):
+    if i == imgs:
+        break
+    src = img.get("src")
+    ext = "jpg"
+    filename = os.path.join(f"images_{query}",str(i)+"."+ext)
+    try:
+        data = urlopen(src).read()
+        open(filename, mode='x').close()
+        with open(filename, mode='wb') as fl:
+            fl.write(data)
+    except Exception as e:
+        i -= 1
+    i += 1
